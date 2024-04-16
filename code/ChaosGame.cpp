5a201467b19bc2d0224bfac3215563ffc73df850
Your Name <you@example.com>
2024-04-16 11:49:08 -0700

    fourth commit

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -115,7 +115,7 @@ int main()
 
         for(int i = 0; i < points.size(); i++)
         {
-            RectangleShape rectUser(Vector2f(10, 10));
+            RectangleShape rectUser(Vector2f(5, 5));
             rectUser.setPosition(Vector2f(points[i].x, points[i].y));
             rectUser.setFillColor(Color::Blue);
             window.draw(rectUser);
